20615aa3c89f48c8e31415ea0d495e30ea131246
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-08-12 19:04:11 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { nanoid } from 'nanoid';
 import { FormThumb } from './ContactForm.styled';
